f2e82b5fe17a2f1d757969e0206376c14d8db516
alirezat66 <alirezataghizadeh66@gmail.com>
2022-01-18 22:46:30 +0300

    add animation to estate_feature_widget

--- lib/ui/views/detail/widgets/estate_feature_widget.dart
+++ lib/ui/views/detail/widgets/estate_feature_widget.dart
@@ -2,22 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:funda_assignment/data/models/estate_detail/characters.dart';
 import 'package:funda_assignment/ui/views/detail/widgets/estate_feature_listview.dart';
 
-class EstateFeatureWidget extends StatelessWidget {
+class EstateFeatureWidget extends StatefulWidget {
   final List<Feature> features;
   const EstateFeatureWidget({Key? key, required this.features})
       : super(key: key);
 
   @override
+  State<EstateFeatureWidget> createState() => _EstateFeatureWidgetState();
+}
+
+class _EstateFeatureWidgetState extends State<EstateFeatureWidget>
+    with TickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation? _expandAnim;
+  bool _isExpanded = false;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    _expandAnim = Tween<double>(
+      begin: 250.0,
+      end: 800.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.bounceOut,
+      ),
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
+    return Stack(
+      alignment: Alignment.bottomCenter,
       children: [
-        Text('Features', style: Theme.of(context).textTheme.headline3),
-        const SizedBox(
-          height: 20,
+        SizedBox(
+          height: _expandAnim!.value != 800 ? _expandAnim!.value : null,
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text('Features', style: Theme.of(context).textTheme.headline3),
+                const SizedBox(
+                  height: 20,
+                ),
+                EstateFeatureListView(features: widget.features)
+              ],
+            ),
+          ),
         ),
-        EstateFeatureListView(features: features)
+        Visibility(
+          visible: !_isExpanded,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white.withOpacity(0.9),
+            child: TextButton(
+                onPressed: () {
+                  _isExpanded = true;
+                  _controller!.forward();
+                },
+                style: TextButton.styleFrom(
+                  minimumSize: Size.zero,
+                  padding: const EdgeInsets.all(8),
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                ),
+                child: const Text('show more')),
+          ),
+        ),
       ],
     );
   }
