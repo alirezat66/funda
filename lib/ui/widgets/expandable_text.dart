e9c87db725f858cb449db637aafe5a17d5dd71bb
alirezat66 <alirezataghizadeh66@gmail.com>
2022-01-18 17:18:05 +0300

    write comment for expandabletext

--- lib/ui/widgets/expandable_text.dart
+++ lib/ui/widgets/expandable_text.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+// in this widget we create expandable animation view in simplest ways.
+// usually we should create this widget with painer but we have not enough time for that.
+// I though that we have a description with more than 5 lines. So if user press more firstly
+// we animated from 5 line to 20 line in 1 second. after that we didnt need it we can show all of lines.
+// I know that it should be changed to worked for all platforms. but it take time as a seprate project.
+// some body use a boolean to show all or some part but I decided to show my animation skills here.
 class ExpandableText extends StatefulWidget {
   final String text;
   const ExpandableText({Key? key, required this.text}) : super(key: key);
